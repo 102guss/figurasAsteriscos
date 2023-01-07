02880466cdb2dfa542238a483d421c7e00fb1ec0
102guss <brown_321@live.com>
2023-01-08 00:26:22 +0100

    Create marco1.cpp
    eliminacion

---
+++ marco1.cpp
@@ -0,0 +1,14 @@
+//marco de asteriscos
+#include<stdio.h>
+int numero,i,j,k;
+
+int main(){
+printf("introducde el numero de renglones del marco:");
+scanf("%d",&numero);		//suponiendo q numero es=5
+
+for(i=1; i<=numero;i++){
+printf("*");	
+}
+
+	
+}// fin del metodo main
