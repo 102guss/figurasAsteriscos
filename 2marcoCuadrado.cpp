4fa7d4f782759bde59de8036d832cefe127d751f
102guss <brown_321@live.com>
2023-01-08 02:35:56 +0100

    Update 2marcoCuadrado.cpp
    finishe

--- 2marcoCuadrado.cpp
+++ 2marcoCuadrado.cpp
@@ -1,11 +1,28 @@
 //marco cuadrado2
 #include<stdio.h>
 int main(){
-printf("hola mundito");	
+int i,j,k,numero;
+printf("Itroduce el numero de lineas del marco: ");
+scanf("%d",&numero);
+
+for(j=0; j<numero;j++){
 	
+	for(i=0; i<numero;i++){
 	
+		if((i==0) || ( j==0) || (i==numero-1) || (j==numero-1)){
+		
+		printf(" *");
+		
+		}//fin del if
+		else{
+		
+		printf("  ");//aqui son dos espacios
+	}
+	}//fin del for
+		printf("\n");
+}
 }//fin del metodo main
 
-
+//hemos terminado
 
 
